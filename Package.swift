d8b5602b183f5e47b92517a91b43dc77f077bff8
AmilaWi <amila.wijayarathna@pickme.lk>
2025-09-23 16:56:49 +0530

    sdwebImagePackageName change

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/AmilaWi/SDWebImage.git", .exact("5.21.2-core2"))
+        .package(url: "https://github.com/AmilaWi/SDWebImage.git", .exact("5.21.3-core2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +25,7 @@ let package = Package(
         .target(
             name: "SDWebImageSwiftUI",
             dependencies: [
-                .product(name: "SDWebImageCore2", package: "SDWebImage")
+                .product(name: "SDWebImageCore2", package: "SDWebImageCore2")
             ],
             path: "SDWebImageSwiftUI",
             sources: ["Classes"],
